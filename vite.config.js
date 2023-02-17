ed8a0862d337d059f7fac67a1b30b5d26a13f90e
Mike Thai <miketh.ai@protonmail.com>
2023-02-17 17:40:03 -0500

    Update package.json
    - add new "local" script for localhost dev  
    package.json - add a build+debug script
    
    
    Local server config 
    
    
    vite vfg

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import dns from 'dns'
 
-// https://vitejs.dev/config/
+dns.setDefaultResultOrder('verbatim')
+
 export default defineConfig({
   plugins: [react()],
+  resolve:{
+    alias:{
+      '~' : path.resolve(__dirname, './src'),
+      '@' : path.resolve(__dirname, './src/Components')
+    },
+  },
+  css: {
+    devSourcemap: true,
+    modules: {
+      localsConvention: 'camelCase'
+    }
+  },
+  build: {
+    sourcemap: true
+  }
 })
